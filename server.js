c149421f57007130a074e1e4482a493f9e3a6352
unknown <jaikaraneeshsundar04@gmail.com>
2025-02-28 20:19:43 +0530

    Making things up-to-data

--- server.js
+++ server.js
@@ -15,6 +15,24 @@ const db = mysql.createConnection({
     database: "employeerecords"
 });
 
+function prepAttendanceRecords(){
+    const sql = 'insert into attendance (empid) select id from employees';
+    db.query(sql, (err, data) => {
+        if (err) {
+            console.log(err.sqlMessage);
+        } else {
+            console.log("employee records prepared to be updated");
+        }
+    });
+}
+
+prepAttendanceRecords();
+setInterval(() => {
+    prepAttendanceRecords();
+}, 3600*1000*24);
+
+
+
 app.get('/users/login', (req,res) => {
     const id = req.query.id;
     const pwd = req.query.pwd;
@@ -34,24 +52,19 @@ app.get('/users/login', (req,res) => {
                 console.log("not a match");
                 res.send("No");
             }
-            
         }
     });
 });
 
 app.get('/putAttendance', (req, res) => {
     const id = req.query.id;
-    const sql = "insert into attendance (empid, record) values ("+id+", 'P')"
+    const sql = "update attendance set record='P' where empid ="+id+" and date = curdate()";
     console.log(id);
     console.log(sql);
     db.query(sql, (err, data) => {
         if (err) {
             console.log(err.sqlMessage);
-            if (err.sqlMessage.slice(0,15)==="Duplicate entry"){
-                res.send("Yes");
-            } else {
-                res.send(err.sqlMessage);
-            }
+            res.send(err.sqlMessage);
         } else {
             res.send("Yes");
         }
@@ -79,19 +92,35 @@ app.get('/users/img', (req,res) => {
     res.sendFile("F:/Projects/gcsprs/node-back/profiles/" + id +".jpg");
 });
 
-function insertEmployees(data){
-    console.log(data);
-    const sql = 'insert into employees (id, name, loc_id, password) values ('+data.empid+', "'+data.name+'", '+data.locid+', "'+data.pwd+'")';
-    db.query(sql, (err, data) => {
+app.get('/reloc', (req, res) => {
+    const id = req.query.id;
+    const locid = req.query.locid;
+    const sql = 'update employees set loc_id = '+locid+' where id = '+id;
+    db.query(sql, (err,data)=>{
         if (err){
-            console.error("Insert Error: "+err.sqlMessage);
-            return "No"+err.sqlMessage;
+            console.log("Update Error: "+err.sqlMessage);
+            res.status(500).send("No");
         } else {
-            console.log("Insertion successful");
-            return "Yes";
+            console.log("Update successful");
+            res.status(200).send("Yes");
         }
     });
-};
+});
+
+app.get('/addloc', (req, res) => {
+    const id = req.query.name;
+    const loc = req.query.loc;
+    const sql = "insert into locations (area, locationStr) values ('"+id+"', '"+loc+"')";
+    db.query(sql, (err,data)=>{
+        if (err){
+            console.log("Insert Error: "+err.sqlMessage);
+            res.status(500).send("No");
+        } else {
+            console.log("Insertion successful");
+            res.status(200).send("Yes");
+        }
+    });
+});
 
 app.post('/addusers', (req, res) => {
     const bb = busboy({ headers: req.headers });
@@ -110,17 +139,27 @@ app.post('/addusers', (req, res) => {
         file.pipe(fs.createWriteStream(des));
         console.log("File successfully written!!");
     });
+
     bb.on("finish", () => {
         console.log("Form handled successfully");
-        const insRes = insertEmployees(data);
-        res.status(200).send("Yes");
+        const sql = 'insert into employees (id, name, loc_id, password) values ('+data.empid+', "'+data.name+'", '+data.locid+', "'+data.pwd+'")';
+        db.query(sql, (err, data) => {
+            if (err){
+                console.error("Insert Error: "+err.sqlMessage);
+                res.send("No "+err.sqlMessage);
+            } else {
+                console.log("Insertion successful");
+                res.send('Yes');
+            }
+        });
     });
+
     bb.on("error", (err) => {
         console.log(err.message);
         res.send("No");
     });
-    req.pipe(bb);
-    
+
+    req.pipe(bb); 
 });
 
 app.listen(3000, ()=>{
